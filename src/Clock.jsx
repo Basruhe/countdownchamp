88bd133f962b37428494b9e21788cc864700e936
Basruhe <sb.ruhe@gmail.com>
2020-03-03 11:40:37 +0100

    added lifecycle hooks to clock

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -14,12 +14,22 @@ class Clock extends Component {
     console.log("Current deadline state, passed as props: ", this.props);
   }
 
+  // If you were to set state in the getTimeUntil function, you'd get infinite loops. You need lifecycle hooks (or methods). You want to calculate before rendering to the screen.
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  // To keep the clock up to date, getTimeUntil needs to keep running (controlled, not infinite!). You use setInterval for this, set at every 1000 ms (=1s).
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
   // Calculate the clock time. First: current date is subtracted from the deadline. Then, this time (in milliseconds!) is converted into days, hours, etc
   // You use the modulo (%) so that you get the remainder. (example: you don't want to see millions of seconds, but only the leftover seconds that are not part of the minutes,hours,etc)
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     console.log("The time:", time);
-    const seconds = Math.floor(time / 1000);
+    const seconds = Math.floor((time / 1000) % 60);
     const minutes = Math.floor((time / 1000 / 60) % 60);
     const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -28,10 +38,18 @@ class Clock extends Component {
     console.log("minutes", minutes);
     console.log("hours", hours);
     console.log("days", days);
+
+    this.setState({
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    });
   }
 
   render() {
-    this.getTimeUntil(this.props.deadline);
+    // // moved to componentwillmount, so that it only updates once. (note; how to get the clock to keep counting down??)
+    // this.getTimeUntil(this.props.deadline);
 
     return (
       <div>
